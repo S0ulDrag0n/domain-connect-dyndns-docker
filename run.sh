7ee4f43824731dd565b0ea46f496619e2d2fff9d
Andy <sd.s0uldrag0n@gmail.com>
2022-01-03 20:05:27 -0800

    Update script to update all domains

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -e setup_done ] ;then
-  /usr/local/bin/domain-connect-dyndns update --domain "$DOMAIN"
+  /usr/local/bin/domain-connect-dyndns update --all
 else
   /usr/local/bin/domain-connect-dyndns setup --domain "$DOMAIN"
   touch setup_done
