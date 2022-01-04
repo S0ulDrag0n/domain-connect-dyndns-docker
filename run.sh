ca723e1f233af5235a65b5af8cbc4000ee12057d
Andy <sd.s0uldrag0n@gmail.com>
2022-01-03 20:11:35 -0800

    Added messages

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 
 if [ -e setup_done ] ;then
   /usr/local/bin/domain-connect-dyndns update --all
+  echo "Updated"
 else
   /usr/local/bin/domain-connect-dyndns setup --domain "$DOMAIN"
   touch setup_done
+  echo "Setup Done"
 fi
