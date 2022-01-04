b481743eb3caedd5c9afcf975d5b72fe8eb39b2a
S0ulDrag0n <SD.S0ulDrag0n@gmail.com>
2022-01-03 22:21:37 -0800

    Added multi domain

--- run.sh
+++ run.sh
@@ -3,8 +3,12 @@ if [ -e setup_done ]; then
   /usr/local/bin/domain-connect-dyndns update --all
   echo 'Updated'
 else
-  echo "Setting up $DOMAIN"
-  /usr/local/bin/domain-connect-dyndns setup --domain "$DOMAIN"
+  echo "Setting up $DOMAIN1"
+  /usr/local/bin/domain-connect-dyndns setup --domain "$DOMAIN1"
+
+  echo "Setting up $DOMAIN2"
+  /usr/local/bin/domain-connect-dyndns setup --domain "$DOMAIN2"
+
   touch setup_done
   echo 'Setup Done'
 fi
