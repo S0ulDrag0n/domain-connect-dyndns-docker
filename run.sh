5bff383f39f7d4333e948472b11f1a93d118a12a
S0ulDrag0n <SD.S0ulDrag0n@gmail.com>
2022-07-22 14:07:13 -0700

    Added support for for multiple domains in env

--- run.sh
+++ run.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
 if [ -e setup_done ]; then
+  for var in "${!DOMAIN@}"; do
+    declare -n ref=$var
+    echo "Found $ref"
+  done
   /usr/local/bin/domain-connect-dyndns update --all --config /app/data/settings.txt
   echo 'Updated'
 else
-  echo "Setting up $DOMAIN1"
-  /usr/local/bin/domain-connect-dyndns setup --domain "$DOMAIN1"
+  for var in "${!DOMAIN@}"; do
+    declare -n ref=$var
+    echo "Setting up $ref"
+    /usr/local/bin/domain-connect-dyndns setup --domain "$ref"
+  done
+  # echo "Setting up $DOMAIN1"
+  # /usr/local/bin/domain-connect-dyndns setup --domain "$DOMAIN1"
 
-  echo "Setting up $DOMAIN2"
-  /usr/local/bin/domain-connect-dyndns setup --domain "$DOMAIN2"
+  # echo "Setting up $DOMAIN2"
+  # /usr/local/bin/domain-connect-dyndns setup --domain "$DOMAIN2"
 
   touch setup_done
   echo 'Setup Done'
